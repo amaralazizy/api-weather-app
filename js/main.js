b1d17b77cd273db74cb5f887520273671e33878d
amaralazizy <121352606+amaralazizy@users.noreply.github.com>
2024-09-28 01:34:13 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ function initAutocomplete() {
 }
 
 let cardsEle = document.querySelectorAll(".card img");
-let googleMap = document.querySelector(".google-map");
+let googleMap = document.querySelector(".google-maps");
 googleMap.setAttribute("src", `https://maps.googleapis.com/maps/api/js?key=${API.GOOGLE_API_KEY}&libraries=places`);
 
 cardsEle.forEach((card) => {
